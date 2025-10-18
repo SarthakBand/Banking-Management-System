4a1262fced8818e1c591679442d4836264f46eed
Sarthak band <157968806+SarthakBand@users.noreply.github.com>
2025-10-19 00:16:05 +0530

    Database Connection
    Database Connected and MySQL setup done

--- src/com/banking/db/DatabaseConn.java
+++ src/com/banking/db/DatabaseConn.java
@@ -1,4 +1,20 @@
 package com.banking.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConn {
+    private static final String URL = "jdbc:mysql://localhost:3306/bankdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "manager";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
